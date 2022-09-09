707a5c2ec18d062dc4e478f1433db69ee18a8bfc
dasaradhamurali <dasaradha.indla.21@mountblue.tech>
2022-09-09 10:46:36 +0530

    new

---
+++ class.js
@@ -0,0 +1,25 @@
+
+
+
+
+let body=document.body
+
+body.innerHTML=`
+ <div class="code">
+ <form>
+     
+     <h3 id="">welcome</h3>
+     <label class="name" for="username">Username</label>
+     <input type="text" id="username" placeholder="Username"><br>
+     <label class="name"  for="password">Password</label>
+     <input type="text" id="password" placeholder="Password"><br>
+     <input  type="submit" class="button">
+     <div> 
+     
+     <a href="https://www.w3schools.com/html/html_css.asp"> forget password</a>
+     </div>
+     
+ </form>
+
+ 
+</div>`
